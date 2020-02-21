8214d6e2009109acc149998dd3db2e5963c42f78
armenbalikyan <armbalikyans@gmail.com>
2020-02-21 11:45:00 +0400

    Updates

---
+++ precache-manifest.318bae18038b8d1a3375c29f6e119df3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c30491557fb0079bbd0c7d3591a4d7",
+    "url": "/Tetris/index.html"
+  },
+  {
+    "revision": "d7510bda69e951ff6885",
+    "url": "/Tetris/static/css/main.e9aa6a1f.chunk.css"
+  },
+  {
+    "revision": "382e4a530b29f0cb41e5",
+    "url": "/Tetris/static/js/2.0348839b.chunk.js"
+  },
+  {
+    "revision": "d7510bda69e951ff6885",
+    "url": "/Tetris/static/js/main.2a7edf34.chunk.js"
+  },
+  {
+    "revision": "9708a83805d881b3530f",
+    "url": "/Tetris/static/js/runtime~main.e25822d4.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/Tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "79bb1b7898c30520ef60143f0d06a91e",
+    "url": "/Tetris/static/media/bg.79bb1b78.png"
+  }
+]);
